d4f2ed9e1ce96d4f9cf109aa45e39afc7846ef5e
Debanuj Barman <debanujbarman.student123@gmail.com>
2025-07-01 15:48:31 +0530

    search doc Update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,13 +7,15 @@ import { TemplatesGallery } from "./templates-gallery";
 
 import { api } from "../../../convex/_generated/api";
 import { DocumentsTable } from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 const Home = () => {
+  const[search] = useSearchParam();
   const {
     results,
     status,
     loadMore
-  } = usePaginatedQuery(api.documents.get,{},{initialNumItems: 5});
+  } = usePaginatedQuery(api.documents.get,{search},{initialNumItems: 5});
 
 
 
